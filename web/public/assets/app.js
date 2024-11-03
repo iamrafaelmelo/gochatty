3c287f96a49e888cbc632b9c8f13ba41fdc12dea
Rafael Melo <52138516+iamrameffort@users.noreply.github.com>
2024-11-03 09:00:28 -0300

    refactor: create and display random user name

--- web/public/assets/app.js
+++ web/public/assets/app.js
@@ -5,6 +5,7 @@ const main = document.getElementById('app');
 
 const websocket = new WebSocket(`ws://${document.location.host}/ws`);
 var pid = null;
+var username = null;
 
 websocket.onmessage = function (event) {
     const data = JSON.parse(event.data);
@@ -12,7 +13,8 @@ websocket.onmessage = function (event) {
     switch (data.type) {
         case 'setup':
             pid = data.pid
-            console.log(pid)
+            username = data.username
+            console.log(data)
             break;
         case 'message':
             append(data);
@@ -32,6 +34,7 @@ form.onsubmit = (event) => {
     const data = JSON.stringify({
         pid: pid,
         type: 'message',
+        username: username,
         message: message.value,
         datetime: new Date().toLocaleTimeString(),
     });
@@ -42,11 +45,10 @@ form.onsubmit = (event) => {
     message.value = '';
 };
 
-
 const append = (data) => {
     list.insertAdjacentHTML('beforeend', `
         <div class="flex gap-y-0.5 flex-col w-full pb-6 ${data.pid === pid ? 'items-end' : 'items-start'}">
-            <p class="text-[0.65rem] text-secondary-500">${data.pid === pid ? 'You' : 'Other'} ${data.datetime}</p>
+            <p class="text-[0.65rem] text-secondary-500">${data.username} &bullet; ${data.datetime}</p>
             <p class="text-sm font-medium ${data.pid === pid ? 'bg-primary-600/35' : 'bg-secondary-600/35'} rounded-md px-3 py-2 gap-y-2 max-w-96">
                 ${data.message}
             </p>
